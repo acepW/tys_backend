c431fbc33cb8fe02e41f47fcebb01bc593144ea4
acepW <acepwahyu001@gmail.com>
2026-05-26 15:40:40 +0700

    up

--- src/services/quotation/quotation.service.js
+++ src/services/quotation/quotation.service.js
@@ -31,7 +31,6 @@ class QuotationService extends DualDatabaseService {
         {
           model: dbModels.Company,
           as: "company",
-          attributes: ["id", "company_name"],
         },
         {
           model: dbModels.Customer,
